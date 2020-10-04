a4998e91a7f6b83673a8a7c11f6b1f8ab6c519a4
Eyar <eyar.gilad@gmail.com>
2020-10-04 21:42:55 +0300

    \

--- src/delivery/delivery.controller.ts
+++ src/delivery/delivery.controller.ts
@@ -24,7 +24,7 @@ export class DeliveryController {
         } catch(err){
             res.send(err);
         }
-        res.send(delivery);
+        res.send('delivery successfully added');
     }
 
     @Get('/get')
